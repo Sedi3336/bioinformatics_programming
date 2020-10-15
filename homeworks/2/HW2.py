6347b2ea5b64452e463cc5bb518bd2b887c3e26e
Sedigheh Fazli <fazli@ohsu.edu>
2020-10-15 22:29:12 +0000

    almost done

--- homeworks/2/HW2.py
+++ homeworks/2/HW2.py
@@ -1,17 +1,37 @@
 import csv
+import sys
+
+def calculate_avg_value(data, column_name):
+    sum_data = 0
+    for item in data:
+        sum_data += item
+    return sum_data/len(data)
 
 def find_average_wines(wine_paths):
+    dict1 = {}
     for wine_path in wine_paths:
-        with open(wine_path) as csv_file:
+        with open("./data/"+wine_path) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
+            dict2 = {}
+            column_names = []
             for row in csv_reader:
                 if line_count == 0:
-                    print(f'Column names are {", ".join(row)}')
+                    for item1 in row:
+                        dict2[item1] = []
+                        column_names.append(item1)
                     line_count += 1
                 else:
-                    print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                    line_count += 1
-            print(f'Processed {line_count} lines.')
+                    for num,item2 in enumerate(row):
+                        dict2[column_names[num]].append(float(item2))
 
-find_average_wines(["data/red_wine_good.csv"])
+            dict3 = {}
+            for k,v in dict2.items():
+                dict3[k] = calculate_avg_value(v, k)
+            dict1[wine_path.replace('.csv','')] = dict3
+    return dict1
+def main():
+    print(find_average_wines(sys.argv[1:]))
+
+if __name__ == "__main__":
+    main()
--- homeworks/2/HW2.sh
+++ homeworks/2/HW2.sh
@@ -26,8 +26,13 @@ cd ../data
 sed 's/;/,/g' ../download/$white > ./$white
 sed 's/;/,/g' ../download/$red > ./$red
 
-awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11 "," $12} ; $12>5 {print $3 "," $5 "," $9 "," $11 "," $12}' $white > white_wine_good.csv
-awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11 "," $12} ; $12<=5 {print $3 "," $5 "," $9 "," $11 "," $12}' $white > white_wine_poor.csv
-awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11 "," $12} ; $12>5 {print $3 "," $5 "," $9 "," $11 "," $12}' $red > red_wine_good.csv
-awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11 "," $12} ; $12<=5 {print $3 "," $5 "," $9 "," $11 "," $12}' $red > red_wine_poor.csv
+awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; $12>5 {print $3 "," $5 "," $9 "," $11}' $white > white_wine_good.csv
+awk -F "," 'NR==2 {print $3 "," $5 "," $9 "," $11} ; $12<=5 {print $3 "," $5 "," $9 "," $11}' $white > white_wine_poor.csv
+awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; $12>5 {print $3 "," $5 "," $9 "," $11}' $red > red_wine_good.csv
+awk -F "," 'NR==2 {print $3 "," $5 "," $9 "," $11} ; $12<=5 {print $3 "," $5 "," $9 "," $11}' $red > red_wine_poor.csv
 
+
+csv_wines=("white_wine_good.csv" "white_wine_poor.csv" "red_wine_good.csv" "red_wine_poor.csv")
+
+cd ..
+python3 HW2.py ${csv_wines[@]}
