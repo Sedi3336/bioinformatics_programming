9af80af2fb2f0646b93b9b2a67dd91e0ecf4e776
Sedigheh Fazli <fazli@ohsu.edu>
2020-10-16 21:49:42 +0000

    final

--- homeworks/2/HW2.py
+++ homeworks/2/HW2.py
@@ -2,35 +2,51 @@ import csv
 import pickle
 import sys
 
+'''
+calculate_avg_value function gets a dictionary of four columns as data and a string to match with the column’s name and calculate the average value for that column.
+data is a dictionary of four columns with the key of a string and value of a list of floats.
+'''
 def calculate_avg_value(data, column_name):
     sum_data = 0
     for item in data:
         sum_data += item
-    return float('%.2f' % (sum_data/len(data)))
+    return ('%.2f' % (sum_data/len(data)))
 
+'''
+find_average_wines function takes some csv file paths in a list as input.
+Then it reads each csv file individually and creates a dictionary(header_value) of that csv file.
+In this dictionary, the key is csv's column names and the values are the corresponding values in each column of the csv file.
+Then this function sends those dictionaries to the calculate_avg_value function and generates a new dictionary( header_avg ) out of the average values for each column.
+At the end, another dictionary (result) is made with the key of wine_paths ( each csv file) and a value of the last dictionary (header_avg) and returns it.
+
+'''
 def find_average_wines(wine_paths):
-    dict1 = {}
+    result = {}
     for wine_path in wine_paths:
         with open("./data/"+wine_path) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
-            dict2 = {}
+            header_value = {}
             column_names = []
             for row in csv_reader:
                 if line_count == 0:
-                    for item1 in row:
-                        dict2[item1] = []
-                        column_names.append(item1)
+                    for header in row:
+                        header_value[header] = []
+                        column_names.append(header)
                     line_count += 1
                 else:
-                    for num,item2 in enumerate(row):
-                        dict2[column_names[num]].append(float(item2))
+                    for num,value in enumerate(row):
+                        header_value[column_names[num]].append(float(value))
 
-            dict3 = {}
-            for k,v in dict2.items():
-                dict3[k] = calculate_avg_value(v, k)
-            dict1[wine_path.replace('.csv','')] = dict3
-    return dict1
+            header_avg = {}
+            for k,v in header_value.items():
+                header_avg[k] = calculate_avg_value(v, k)
+            result[wine_path.replace('.csv','')] = header_avg
+    return result
+'''
+sys.argv is a list in Python, which contains the command-line arguments passed to the script.
+Pickle Python module is used to save the resulting dictionary to a file in a directory called results
+'''
 def main():
     results = find_average_wines(sys.argv[1:])
     print("Here are the avreages for different wine types and qualities:")
@@ -40,5 +56,9 @@ def main():
     with open('./results/results.pickle', 'wb') as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+'''
+This common piece of code is used to run the main function when my script is called from the command line (in this case bash script)
+'''
 if __name__ == "__main__":
     main()
+
--- homeworks/2/HW2.sh
+++ homeworks/2/HW2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#This is Sedi's reply to the first homework of the Bioinformatics programming course
+#This is Sedi's reply to the second homework of the Bioinformatics programming and scripting course
 
 white="winequality-white.csv"
 red="winequality-red.csv"
 url="http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/"
-
+# A directory called download is created and we change our directory to download
 if [ ! -e "./download/" ];then
 	mkdir download
 fi
 cd download
-
+#wget is used to download the data (white and red) in the download directory.
 if [ ! -e $white ];then
 	wget $url$white
 fi
 if [ ! -e $red ];then
 	wget $url$red
 fi
-
+#a directory called data is created and we change our current directory to data and replace all the ; to , and save changes in the data directory
 if [ ! -e "../data" ];then
 	mkdir ../data
 fi
@@ -25,15 +25,15 @@ cd ../data
 
 sed 's/;/,/g' ../download/$white > ./$white
 sed 's/;/,/g' ../download/$red > ./$red
+# awk is used to keep only the 4 columns with the variables of interest and quality is used to subset the data.
 
-awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; $12>5 {print $3 "," $5 "," $9 "," $11}' $white > white_wine_good.csv
-awk -F "," 'NR==2 {print $3 "," $5 "," $9 "," $11} ; $12<=5 {print $3 "," $5 "," $9 "," $11}' $white > white_wine_poor.csv
-awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; $12>5 {print $3 "," $5 "," $9 "," $11}' $red > red_wine_good.csv
-awk -F "," 'NR==2 {print $3 "," $5 "," $9 "," $11} ; $12<=5 {print $3 "," $5 "," $9 "," $11}' $red > red_wine_poor.csv
+awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; NR==1 { next } $12>5 {print $3 "," $5 "," $9 "," $11}' $white > white_wine_good.csv
+awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; NR==1 { next } $12<=5 {print $3 "," $5 "," $9 "," $11}' $white > white_wine_poor.csv
+awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; NR==1 { next } $12>5 {print $3 "," $5 "," $9 "," $11}' $red > red_wine_good.csv
+awk -F "," 'NR==1 {print $3 "," $5 "," $9 "," $11} ; NR==1 { next } $12<=5 {print $3 "," $5 "," $9 "," $11}' $red > red_wine_poor.csv
 
-
+# a list of wine paths and a directory called results are created here. At the end, we run the python  which will perform the analysis on the csv_wines
 csv_wines=("white_wine_good.csv" "white_wine_poor.csv" "red_wine_good.csv" "red_wine_poor.csv")
-
 cd ..
 if [ ! -e "./results/" ];then
         mkdir results
